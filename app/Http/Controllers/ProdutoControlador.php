992d8ecbcce20c5ef88d092774e948a132fe37f1
RogerioPires011 <44650582+RogerioPires011@users.noreply.github.com>
2019-02-25 17:09:39 -0400

    initial commit

---
+++ app/Http/Controllers/ProdutoControlador.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControlador extends Controller
+{
+    pulic function listar(){
+    	$produtos=[
+         "Notebook Asus 17GB",
+         "Mouse e teclado Micro USB",
+         "Monitor 21-samsung",
+         "Impressora HP",
+         "Disco SSD 512 GB"
+    	]; 
+
+    }
+ }
--- resources/views/pagina.blade.php
+++ resources/views/pagina.blade.php
@@ -1,17 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link href="{{asset('css/app.css')}}" rel="stylesheet">
+	<!--<link href="{{asset('css/app.css')}}" rel="stylesheet">-->
+	<link href="{{URL::to('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
+   
+    @alerta(['tipo'=>'danger','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
 
-    @component('components.meucomponente')
-    <strong>Erro: </strong> Sua mensagem de erro.
-    @endcomponent 
 
-	
-    <script src="{{asset('js/app.js')}}" type="text/javascript"> 
-    </script>
+     @alerta(['tipo'=>'warning','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
+
+
+     @alerta(['tipo'=>'success','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
+
+
+     @alerta(['tipo'=>'primary','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
+
+
+     @alerta(['tipo'=>'secondary','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
+
+
+     @alerta(['tipo'=>'info','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
+
+
+     @alerta(['tipo'=>'dark','titulo'=>'Erro fatal'])
+    <strong>Erro:</strong> Sua mensagem de erro.		
+     @endcomponent
+
+    <script src="{{URL::to('js/app.js')}}" type="text/javascript"></script>
+    <!--<script src="{{asset('js/app.js')}}" type="text/javascript"> 
+    </script>-->
 
 </body>
 </html>
